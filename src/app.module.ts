ff29956c96290410a3aad7701d3f00b0c9b119f6
Helena Onezia Lewi <2381003@unai.edu>
2025-03-12 22:15:23 +0700

    handson7 songs

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SongsController } from './songs/songs.controller';
+import { SongsModule } from './songs/songs.module';
+import { SongsService } from './songs/songs.service';
+
+@Module({
+  imports: [SongsModule],
+  controllers: [AppController],
+  providers:  [AppService],
+})
+export class AppModule {}
---
+++ src/songs/create-song.dto.ts
@@ -0,0 +1,4 @@
+export class CreateSongDTO /*Data Transfer Object*/{
+    title: string;
+    artist: string;
+} 
